464b28bcf5f7b418d17fe6ee78773c0a892406de
Rohit Chand <chandrohitsumit@gmail.com>
2025-08-25 12:44:36 +0530

    Updated project files with latest changes

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -13,7 +13,7 @@ function Signup() {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4001/user/signup",
+        "https://todo7-9miu.onrender.com/user/signup",
         {
           username,
           email,
