64caeffd7f23fb0a1430b22efc4a89e5b05832ff
rchand7 <150305815+rchand7@users.noreply.github.com>
2025-09-01 15:30:13 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://todo7-63n1.onrender.com/user/login",
+        "https://todo7-9miu.onrender.com/user/login",
         {
           email,
           password,
